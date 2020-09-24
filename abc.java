b168983e2874472a1fbf2a967bdfdfe5265cee5d
humaira-farewar <71778943+humaira-farewar@users.noreply.github.com>
2020-09-24 08:00:35 +0430

    abc-2.0
    it finds the factorial for any given number.

--- abc.java
+++ abc.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 class abc
 {
 public static void main(String args[])
 {
-System.out.println("Hello World"); 
-System.out.println("What's up people"); 
-System.out.println(" hkjkgkfhd "); 
+int n,c, fact = 1;
+System.out.println("Enter a number");
+Scanner sct = new Scanner(System.in);
+n = sct.nextInt();
+if ( n < 0 )
+System.out.println("Number should be non-negative.");
+else
+{
+for ( c = 1 ; c <= n ; c++ )  // Calculating Factorial
+fact = fact*c;
+System.out.println("Factorial of "+n+" is = "+fact);
+}
 }
 }
